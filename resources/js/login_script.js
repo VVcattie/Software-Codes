571a9fb730a293552487e829a9f0269af6cf80ab
Keyuan125 <31080277+Keyuan125@users.noreply.github.com>
2019-10-26 15:34:00 -0600

    Update login_script.js

--- resources/js/login_script.js
+++ resources/js/login_script.js
@@ -3,3 +3,13 @@
 function loginButton(){
 	document.body.style.backgroundColor = 'red';
 }
+
+function validatePassword() {
+	var password = document.getElementById("passwordInput").value;
+	var confirmPassword = document.getElementById("confirmPasswordInput").value;
+	if (password != confirmPassword) {
+		alert("The password you enter doesn't match! Please try again");
+		return false;
+	}
+	else return true;
+}

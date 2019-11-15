72b713150ca168143141fcf7f5114099b358d2da
wesleyshen2000 <wesley.shen.2000@gmail.com>
2019-11-14 17:29:44 -0700

    Added node.js to master branch

--- resources/js/login_script.js
+++ resources/js/login_script.js
@@ -7,6 +7,38 @@ function loginButton(){
 // function validatePassword() {
 // 	var password = document.getElementById("passwordInput").value;
 // 	var confirmPassword = document.getElementById("confirmPasswordInput").value;
+// 	if (password != confirmPassword) {
+// 		alert("The password you enter doesn't match! Please try again");
+// 		return false;
+// 	}
+// 	else return true;
+// }
+
+// //the following functions check to see if an email is valid, the first performs the check, the second will display a message
+// function validateEmail(email) {
+//   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(email);
+// }
+
+// function validate() {
+//   var $result = $("#result");
+//   var email = $("#email").val();
+//   $result.text("");
+
+//   if (validateEmail(email)) {
+//     $result.text(email + " is valid :)");
+//     $result.css("color", "green");
+//   } else {
+//     $result.text(email + " is not valid :(");
+//     $result.css("color", "red");
+//   }
+//   return false;
+// }
+// $("#validate").on("click", validate);
+
+// function validatePassword() {
+// 	var password = document.getElementById("passwordInput").value;
+// 	var confirmPassword = document.getElementById("confirmPasswordInput").value;
 // 	var match = document.getElementById("match");
 //
 // 	if (password != confirmPassword) {

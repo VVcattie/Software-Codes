f0e28facc38a43d1b799fb33af8287cfaea03a4f
Keyuan125 <31080277+Keyuan125@users.noreply.github.com>
2019-12-02 16:04:30 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/')); // This line is necessary for us to use relative paths and access our resources directory
 
 
-// login page
+// below are the basic functions to render all of the pages as they are called
+//this uses the .pug pages
 app.get('/loginPage', function(req, res) {
 	res.render('loginPage',{
 		my_title:"Login Page"
@@ -56,66 +57,130 @@ app.get('/signUp', function(req, res){
     my_title:"Sign Up"
   });
 });
+app.get('/paymentPage', function(req, res){
+  res.render('paymentPage',{
+    my_title:"Payment Info"
+  });
+});
+
+//function to see if the account exists
 app.get('/loginPage/checkUserLogin', function(req, res){
+  var validUser = false;
 	db.any('SELECT*FROM users;')
 		.then(function(rows){
+      console.log("Username entered" + req.query.username);
+      console.log("Password entered" + req.query.password);
 			for (var i=0;i < rows.length;i++){
 		if (rows[i].user_username == req.query.username && rows[i].user_password == req.query.password){
-			console.log("login success");
+			validUser = true;
 		}
-		else{
-			console.log("failed")
-		}
-	}
+	 }
+   if (validUser){
+     console.log("login success");
+     return res.redirect("/homePage");
+   }
+   else{
+     console.log("login failure");
+     return res.redirect("/loginPage");
+   }
 		})
 		.catch(function(err){
 			console.log(err);
 		});
 });
-
-app.post('/signUp/createAccount', function(req,res) {
+//function to post a new account to the database
+app.post('/signUp/createAccount', function(req,res){
   var firstNameInput = req.body.firstNameInput;
-  var lastNameInput = req.body.LastNameInput;
-  var usernameInput = req.body.usernameInput;
+	var lastNameInput = req.body.LastNameInput;
+	var usernameInput = req.body.usernameInput;
   var passwordInput = req.body.passwordInput;
-  var emailInput = req.body.emailInput;
-  // var createUser = {
-  // 	user_username: req.body.usernameInput,
-  // 	user_password: req.body.passwordInput,
-  // 	user_email: req.body.emailInput,
-  // 	user_first_name: req.body.firstNameInput,
-  // 	user_last_name: req.body.lastNameInput
-  // }
-	let insert_statement = "INSERT INTO users(user_username, user_password, user_email, user_first_name, user_last_name) VALUES('" + usernameInput + "','" + passwordInput + "','" + emailInput +"','" + firstNameInput +"','" +lastNameInput +"') ON CONFLICT DO NOTHING;";
-	db.any(insert_statement)
-		.then(function(){
-			console.log('success added!')
-		})
+  var emailInput = req.body.emailInput; 
+
+	var insert_statement = "INSERT INTO users(user_id, user_password, user_email, user_first_name, user_last_name) VALUES('" + 1 + "','" + passwordInput + "','" + emailInput +"','" + firstNameInput +"','" +lastNameInput +"') ON CONFLICT DO NOTHING;";
+
+	//var user_select = 'select * from users;';
+	db.task('get-everything', task => {
+		console.log(usernameInput)
+        console.log(passwordInput)
+        task.batch([
+            task.any(insert_statement),
+            task.any(user_select)
+        ]);
+        return res.redirect("/loginPage")
+    })
+    .catch(error => {
+        // display error message in case an error
+            console.log('error'); //if this doesn't work for you replace with console.log
+            res.render('signUp', {
+                title: 'Sign Up',
+            })
+    });
+
+});
+//function to render the listings of textbooks from the database
+app.get('/listings', function(req, res){
+    //check to see if there is a selection, then do the db calls
+    var category = req.query.myList.value;
+    if (category){
+      var callListings = "select * from listings where listing_category = "+ category + ";";
+    }
+    else{
+      var callListings = "select * from listings;";
+    }
+    var listingUser = "select user_username from users right join listings on listing_user_id = user_id;";
+    db.task('get-everything', task => {
+          return task.batch([
+              task.any(callListings),
+              task.any(listingUser)
+          ]);
+      })
+      .then(info => {
+        res.render('/listings',{
+          my_title: "Listings",
+          bookTitle: info[0].listing_title,
+          description: info[0].listing_description,
+          listingCategory: info[0].listing_category,
+          bookType: info[0].listing_booktype,
+          price: info[0].listing_price,
+          condition: info[0].listing_condition,
+          author: info[0].listing_author,
+          listingUsername: info[1]
+        })
+      })
 		.catch(function(err){
 			console.log(err);
 		});
-
-    // .then(info => {
-    // 	res.render('pages/home',{
-		// 		my_title: "Home Page",
-		// 		data: info[1],
-		// 		color: color_hex,
-		// 		color_msg: color_message
-		// 	})
-    // })
-    // .catch(error => {
-    //     // display error message in case an error
-    //         console.log('error'); //if this doesn't work for you replace with console.log
-    //         res.render('signUp', {
-    //             title: 'Sign Up',
-    //             data: '',
-    //             color: '',
-    //             color_msg: ''
-    //         })
-    // });
-
 });
+//function to add a new textbook or notebook listing to the database
+app.post('/listings/postListing', function(req,res){
+    var title = req.body.title;
+    var category = req.body.subject;
+    var description = req.body.description;
+    var bookType = req.body.type;
+    var price = req.body.price;
+    var username = req.body.sellerName;
+    var email = req.body.sellerEmail;
+    var getUserID = "select user_id from users where user_email = " + email + ";";
+    var userId = "";
 
+    db.any(getUserID)
+  		.then(function(rows){
+        userID = rows;
+      });
+    var insertStatement = "declare @id uniqueidentifier set @id = NEWID() INSERT INTO listings(listing_id, listing_title, listing_user_id, listing_description, listing_category, listing_booktype, listing_price) VALUES(@id, "+ title + "," + userID + ", " + description + "," + category + "," + bookType + ","+price+") ON CONFLICT DO NOTHING;";
+    db.task('get-everything', task => {
+          return task.batch([
+              task.any(insertStatement)
+          ]);
+      })
+      .catch(error => {
+          // display error message in case an error
+              console.log('error'); //if this doesn't work for you replace with console.log
+              res.render('/listings', {
+                  title: 'Listings',
+              })
+      });
+});
 
 
 app.listen(3000);
